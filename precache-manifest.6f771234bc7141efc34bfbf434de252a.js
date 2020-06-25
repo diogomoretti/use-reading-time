f780af49f1d41784bb3348df86b22a68d938f5ae
Diogo Moretti <diogo.pearljam@gmail.com>
2020-06-25 19:29:28 -0300

    Updates

---
+++ precache-manifest.6f771234bc7141efc34bfbf434de252a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d2a9ab8a9a3f4ddbac6dc1cbd4c4cc6",
+    "url": "/use-reading-time/index.html"
+  },
+  {
+    "revision": "abb672087bcbc92a1787",
+    "url": "/use-reading-time/static/css/main.022ddbf2.chunk.css"
+  },
+  {
+    "revision": "5c3a5ae3b79f7e66de26",
+    "url": "/use-reading-time/static/js/2.58da2948.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/use-reading-time/static/js/2.58da2948.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "abb672087bcbc92a1787",
+    "url": "/use-reading-time/static/js/main.ebafa390.chunk.js"
+  },
+  {
+    "revision": "de810e27ac89c586f64f",
+    "url": "/use-reading-time/static/js/runtime-main.8da4a1ef.js"
+  }
+]);
